6a85bcd0e44bf341403c239f33092f2ecf9bdf46
Gery <you@example.com>
2024-02-11 13:23:02 +0100

    first commit

---
+++ src/projet/java/groupe/pkg6/ProjetJavaGroupe6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet.java.groupe.pkg6;
+
+/**
+ *
+ * @author Gery GUEDEGBE
+ */
+public class ProjetJavaGroupe6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
